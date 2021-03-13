c540db4e029e4c6e49d3873fd81e5c6627da0cf4
freemt <yucongo+fmt@gmail.com>
2021-03-14 02:06:02 +0800

    Publish version 0.1.0

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # deepl-grpc
 [![tests](https://github.com/ffreemt/deepl-grpc/actions/workflows/routine-tests.yml/badge.svg)][![python](https://img.shields.io/static/v1?label=python+&message=3.7%2B&color=blue)](https://img.shields.io/static/v1?label=python+&message=3.7%2B&color=blue)[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)[![PyPI version](https://badge.fury.io/py/deepl-grpc.svg)](https://badge.fury.io/py/deepl-grpc)
 
-deepl grpc, server and client
+deepl grpc, server and client, cross platform (Windows/MacOS/Linux) 
 
 ## Installation
 *   Install ``grpc-reflection``
--- deepl_grpc/deepl_client.py
+++ deepl_grpc/deepl_client.py
@@ -36,11 +36,7 @@ class DeeplClient(object):
 
     def get_url(self, message, from_lang="auto", to_lang="zh"):
         """Client function to call the rpc for GetServerResponse."""
-        message = pb2.Message(
-            message=message,
-            from_lang=from_lang,
-            to_lang=to_lang,
-        )
+        message = pb2.Message(message=message, from_lang=from_lang, to_lang=to_lang,)
 
         logger.debug("message: %s", message)
 
@@ -56,25 +52,26 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         to_lang = sys.argv[1]
 
-    print(" Default destination language: zh. "
-    "Howere, you can supply to any of English (en), German (de), French (fr), Spanish (es), Portuguese (pt), Dutch (nl), Italian (it), Polish (pl), Russian (ru), Japanese (ja), and Chinese (zh) as the second argment, e.g., python deepl_grpc/deepl_client.py de")
+    print(
+        " Default destination language: zh. "
+        "Howere, you can supply to any of English (en), German (de), French (fr), Spanish (es), Portuguese (pt), Dutch (nl), Italian (it), Polish (pl), Russian (ru), Japanese (ja), and Chinese (zh) as the second argment, e.g., python deepl_grpc/deepl_client.py de"
+    )
     print("")
 
     message = ""
     # for elm in range(1):
     while True:
-        message = input(" Type something to translate (quit  or ctrl-C to exit): ").strip()
+        message = input(
+            " Type something to translate (quit  or ctrl-C to exit): "
+        ).strip()
         if not message:
             continue
-        if message[:4].lower() in ['quit']:
+        if message[:4].lower() in ["quit"]:
             break
 
         try:
-            result = client.get_url(
-                message=message,
-                to_lang=to_lang,
-            )
+            result = client.get_url(message=message, to_lang=to_lang,)
             # print(f"{elm}: {result}", result.message)
-            print(message, '->', result.message)
+            print(message, "->", result.message)
         except Exception as exc:
             logger.error(exc)
